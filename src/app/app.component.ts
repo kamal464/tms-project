8d34fe761bd661e91b9986638c4ec4a5be909356
unknown <kollimarlakamal@outlook.com>
2023-03-29 18:43:01 +0530

    kamal updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { OrgformComponent } from './orgform/orgform.component';
 import { MatCardModule } from '@angular/material/card';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { FormBuilder, FormGroup,FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -10,21 +12,76 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'org';
-
+  empForm: FormGroup;
   display = false;
   onPress() {
     this.display = true;
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private _fb: FormBuilder,) {
 
+      this.empForm = this._fb.group({ 
+        name:new FormControl(''),
+        shortname:new FormControl(''),
+        // country:new FormControl(''),
+        displayname:new FormControl(''  ),
+        phone:new FormControl(''),
+        email:new FormControl(''),
+        fax:new FormControl(''),
+        website:new FormControl(''),
+        whatsapp:new FormControl(''),
+        linkedin:new FormControl(''),
+        comments:new FormControl(''),
+      })
+
+
+    } 
+
+    
+///get method
+  
   ngOnInit(): void {
-    this.http
-      .get('https://fakestoreapi.com/products/1',)
-      .subscribe((data) => {
-        console.log(data);
-      }); 
+    
+   this.getRecord();
+   
   }
 
- 
+  getRecord() {
+    
+    this.http
+      .post('http://192.168.0.55:5000/org/getorg',{})
+      .subscribe((data) => {
+        this.empForm.patchValue(data);
+        console.log(data);
+      }); 
+
+}
+
+
+
+// delete method
+deleteItem(){
+  return this.http.delete('http://192.168.0.55:5000/deleteorg')
+}
+onDelete() {
+  this.deleteItem().subscribe((data)=>{
+    this.empForm.patchValue(data);
+console.log("field is deleted",data)
+  })
+}
+
+
+
+
+//put method 
+
+update() {
+  const formData = this.empForm.value;
+
+  this.http.put('http://192.168.0.55:5000/orgupdate', formData).subscribe((user) => {
+    this.empForm.patchValue(user);
+    console.log('field is updated')
+  });
+}
 }
--- src/app/orgform/orgform.component.ts
+++ src/app/orgform/orgform.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup,FormControl} from '@angular/forms';
+import {serialize} from 'form-serialize';
+import { countries } from '../shared/models/country.model';
+import { OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-orgform',
   templateUrl: './orgform.component.html',
@@ -15,11 +19,11 @@ export class OrgformComponent {
       this.empForm = this._fb.group({ 
         name:new FormControl(''),
         shortname:new FormControl(''),
-        country:new FormControl(''),
-        // displayname:'',
+        // country:new FormControl(''),
+        displayname:new FormControl(''  ),
         phone:new FormControl(''),
-        fax:new FormControl(''),
         email:new FormControl(''),
+        fax:new FormControl(''),
         website:new FormControl(''),
         whatsapp:new FormControl(''),
         linkedin:new FormControl(''),
@@ -28,13 +32,39 @@ export class OrgformComponent {
 
 
     } 
+
+    
+
   onSubmit(formdata) {
+    let jsonform = JSON.stringify(this.empForm.value);
+     
+    console.log(jsonform)
+
     this.http
-      .post('http://127.0.0.1:5000/org',{})
+      .post('http://192.168.0.55:5000/org/addorg',jsonform)
       .subscribe((response) => {
         console.log(response);
       });
+  // 
+  // onSubmit(formdata) {
+  //   this.http
+  //     .get('http://192.168.0.55:5000/getorg')
+  //     .subscribe((response) => {
+  //       console.log(response);
+  //     });
   }
 
 
 }
+// export class YourComponent implements OnInit {
+  
+  
+  
+//   constructor(
+//     private https:countries
+//   ){}
+
+//   ngOnInit(): void {
+
+//   }
+// }
